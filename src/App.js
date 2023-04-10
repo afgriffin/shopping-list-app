f316ff193ef0dd2084ca49c617ec850215156d81
Allie <afgriffin91@gmail.com>
2023-04-10 15:49:43 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Suspense, lazy } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import AppContext from './context/AppContext';
 import Header from './components/Header/Header';
-import Lists from './pages/Lists';
-import ListDetail from './pages/ListDetail';
-import ListForm from './pages/ListForm';
+
+const Lists = lazy(() => import(/* webpackChunkName: "Lists" */ './pages/Lists'));
+const ListDetail = lazy(() => import(/* webpackChunkName: "ListDetail" */ './pages/ListDetail'));
+const ListForm = lazy(() => import(/* webpackChunkName: "ListForm" */ './pages/ListForm'));
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -29,11 +32,15 @@ function App() {
       <AppWrapper>
         <BrowserRouter>
           <Header />
-          <Routes>
-            <Route path='/' element={<Lists />} />
-            <Route path='/list/:listId/new' element={<ListForm />} />
-            <Route path='/list/:listId' element={<ListDetail />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <AppContext>
+              <Routes>
+                <Route path='/' element={<Lists />} />
+                <Route path='/list/:listId/new' element={<ListForm />} />
+                <Route path='/list/:listId' element={<ListDetail />} />
+              </Routes>
+            </AppContext>
+          </Suspense>
         </BrowserRouter>
       </AppWrapper>
     </>
